0dcedf4047af385a9b2168febc96e78ffddb6a7f
Oleeze <orudenkony@gmail.com>
2017-03-05 20:24:31 -0500

    Update dbh.php

--- dbh.php
+++ dbh.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost", "id352323_oleeze", "123456", "id352323_register");
+$conn = mysqli_connect(//"localhost", "id", "password" , "name" );
 
 if (!$conn) {
 	die("Connection failed: ".mysqli_connect_error());
